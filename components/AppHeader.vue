df27c992e5222f51b23af8e741adcb525ad3b768
Alex Rapley <rapley3@gmail.com>
2019-06-24 20:06:59 +1000

    Add WIP dark section based on route

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,29 +1,74 @@
 <template>
-  <header class="column h-padded container">
-    <div class="horizontal upper">
-      <h1>zeenia</h1>
-      <nuxt-link :to="{ path: '/about'}" tag="span" class="link">
-        about me
-      </nuxt-link>
-    </div>
+  <header :class="{ dark: isDark }" class="column h-padded container">
+    <h1 class="horizontal">
+      zeenia
+    </h1>
+
     <h1 class="horizontal">
       kaur
     </h1>
+
+    <div class="nav">
+      <nuxt-link v-if="!isDark" :to="{ path: '/about'}" tag="span" class="link">
+        about me
+      </nuxt-link>
+
+      <nuxt-link v-else :to="{ path: '/'}" tag="span" class="link">
+        close
+      </nuxt-link>
+    </div>
   </header>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'AppHeader',
+  data() {
+    return {
+      isDark: false
+    }
+  },
+  watch: {
+    $route: {
+      handler(val) {
+        if (val.path === '/about') {
+          this.isDark = true
+        } else {
+          this.isDark = false
+        }
+      },
+      immediate: true
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 header {
   margin-bottom: 9rem;
+  position: relative;
+
+  &.dark {
+    background-color: $black;
+    color: #fff;
+    margin: 0;
+    padding-bottom: 4rem;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+
+    h1 {
+      color: #fff;
+      font-size: 2rem;
+      width: 20%;
+    }
+  }
 }
 
-.upper {
-  justify-content: space-between;
-  align-items: center;
+.nav {
+  position: absolute;
+  right: 1rem;
+  top: 1rem;
 }
 
 h1 {
