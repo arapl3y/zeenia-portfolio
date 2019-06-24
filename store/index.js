a3ac831ea8b2e06e3f4e2f4669a81b353a8db01a
Alex Rapley <rapley3@gmail.com>
2019-06-24 18:54:13 +1000

    Add WIP about to CMS

--- store/index.js
+++ store/index.js
@@ -3,18 +3,23 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export const state = () => ({
-  projectPosts: []
+  projectPosts: [],
+  aboutData: []
 })
 
 export const mutations = {
   SET_PROJECTS(state, data) {
     state.projectPosts = data
   }
+  // SET_ABOUT(state, data) {
+  //   state.aboutData = data
+  // }
 }
 
 export const actions = {
   async nuxtServerInit({ dispatch }) {
     await dispatch('getProjectPosts')
+    // await dispatch('getAboutData')
   },
   async getProjectPosts({ state, commit }) {
     const context = await require.context(
@@ -30,4 +35,14 @@ export const actions = {
 
     commit('SET_PROJECTS', searchposts.reverse())
   }
+  // async getAboutData({ state, commit }) {
+  //   const context = await require.context('~/content/about/', false, /\.json$/)
+
+  //   const about = await context.keys().map(key => ({
+  //     ...context(key),
+  //     _path: `/projects/${key.replace('.json', '').replace('./', '')}`
+  //   }))
+
+  //   commit('SET_ABOUT', about.reverse())
+  // }
 }
