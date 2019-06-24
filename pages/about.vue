a1fe19f3163c52cbec6a9270b6232cbcd9ee8f8e
Alex Rapley <rapley3@gmail.com>
2019-06-24 20:07:05 +1000

    Add WIP about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,77 @@
 <template>
-  <div>about page</div>
+  <div class="about container">
+    <div class="row h-padded">
+      <p>{{ aboutData.description }}</p>
+    </div>
+
+    <img :src="aboutData.thumbnail" class="profile-image">
+
+    <div class="row h-padded">
+      <p>{{ aboutData.industries }}</p>
+    </div>
+
+    <div class="row h-padded">
+      <p>{{ aboutData.experience }}</p>
+    </div>
+
+    <div class="row h-padded">
+      <p>{{ aboutData.hobbies }}</p>
+    </div>
+
+    <div class="row h-padded">
+      <p>{{ aboutData.social }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'About',
+  computed: {
+    aboutData() {
+      return this.$store.state.aboutData[0]
+    }
+  }
+}
 </script>
 
-<style>
+<style scoped lang="scss">
+.about {
+  background-color: $black;
+  color: #fff;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 4rem;
+  max-width: 50rem;
+
+  & .row:nth-of-type(odd) {
+    justify-content: flex-start;
+  }
+
+  & .row:nth-of-type(even) {
+    justify-content: flex-end;
+  }
+}
+
+.row {
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  p {
+    max-width: 15rem;
+    font-size: 1.5rem;
+    margin: 0;
+    z-index: 1;
+  }
+}
+
+.profile-image {
+  max-width: 25rem;
+  position: absolute;
+  top: 15rem;
+  z-index: 0;
+}
 </style>
