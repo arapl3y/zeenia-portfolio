4c673743cc83e284f60ebec00d3228f1b2d69389
Alex Rapley <rapley3@gmail.com>
2019-06-30 19:52:01 +1000

    Add project profile route

---
+++ pages/projects/_slug.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{ project.title }}</h2>
+    <p>{{ project.body }}</p>
+    <img :src="project.thumbnail" >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectProfile',
+  computed: {
+    project() {
+      return this.$store.getters.getProjectBySlug(this.$route.params.slug)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
