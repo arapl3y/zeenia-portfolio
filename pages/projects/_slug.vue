102adc508d6bed570372cf410e275e0729a23a5a
arapl3y <rapley3@gmail.com>
2019-07-09 23:45:10 +1200

    Add random next project to profile page

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
+  <div class="container h-padded">
     <h2>{{ project.title }}</h2>
     <p>{{ project.body }}</p>
-    <img :src="project.thumbnail" >
+    <div class="img-container">
+      <img :src="project.thumbnail">
+    </div>
+
+
+    <div class="suggestions">
+      <p>Something else to check out</p>
+
+      <img :src="randomProject.thumbnail">
+      <p>{{ randomProject.title }}</p>
+    </div>
   </div>
 </template>
 
@@ -12,10 +22,35 @@ export default {
   computed: {
     project() {
       return this.$store.getters.getProjectBySlug(this.$route.params.slug)
+    },
+    randomProject() {
+      return this.$store.getters.getRandomProject
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+h2 {
+  margin-bottom: 10rem;
+}
+
+.img-container {
+  margin-top: 8rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    margin: 0 auto;
+  }
+}
+
+.suggestions {
+  margin-top: 10rem;
+
+  img {
+    width: 30rem;
+  }
+}
 </style>
