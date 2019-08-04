043e23fbb4063b32468539224f45e7ca126e7850
arapl3y <rapley3@gmail.com>
2019-08-04 21:17:52 +1000

    Add custom loading component

---
+++ components/loading.vue
@@ -0,0 +1,46 @@
+<template>
+  <div v-if="loading" class="loading-container">
+    <div class="loading-circle" />
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    loading: false
+  }),
+  methods: {
+    start() {
+      this.loading = true
+    },
+    finish() {
+      this.loading = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.loading-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: #fff;
+  z-index: 999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.loading-circle {
+  display: block;
+  width: 50px;
+  height: 50px;
+  background-color: #fff;
+  border-radius: 50%;
+  background-color: #ffd874;
+  animation: pulse-circle 1.25s cubic-bezier(0.455, 0.03, 0.515, 0.955) -0.4s infinite;
+}
+</style>
