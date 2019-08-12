b3e9203d8da7a3448b0ecc81c667df6d1235a25c
arapl3y <rapley3@gmail.com>
2019-08-12 23:57:34 +1000

    Update meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'zeenia kaur',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -19,7 +19,7 @@ module.exports = {
   /*
   ** Customize the progress-bar color
   */
-  loading: '~/components/loading.vue',
+  loading: '~/components/Loading.vue',
 
   /*
   ** Global CSS
